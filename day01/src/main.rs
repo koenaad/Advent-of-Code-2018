0aca310a1fc7137279fd90b55373cb1b645a18bb
Koenraad Verheyden <mail@koenraadverheyden.com>
2018-12-01 12:46:30 +0100

    Solution for day 1

---
+++ day01/src/main.rs
@@ -0,0 +1,67 @@
+extern crate structopt;
+
+use std::collections::HashSet;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    /// Starting frequency
+    start: i32,
+
+    /// Input file with changes
+    #[structopt(parse(from_os_str))]
+    input_file: std::path::PathBuf,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+
+    let start = opt.start;
+    let input = parse_file(&opt.input_file);
+
+    // Puzzle 1
+    let mut result = start;
+
+    for num in &input {
+        result = result + num;
+    }
+    println!("*** Puzzle 1: end result = {}", result);
+
+    // Puzzle 2
+    let mut curr = opt.start;
+    let mut prev_freqs: HashSet<i32> = HashSet::new();
+    let mut found = false;
+
+    prev_freqs.insert(curr);
+
+    while !found {
+        for num in &input {
+            curr = curr + num;
+
+            if prev_freqs.contains(&curr) {
+                found = true;
+                break;
+            }
+
+            prev_freqs.insert(curr);
+        }
+    }
+    println!("*** Puzzle 2: first duplicate frequency = {}", curr);
+}
+
+/// panics if there is an issue reading or parsing `input_file`
+fn parse_file(input_file: &std::path::PathBuf) -> Vec<i32> {
+    let content = std::fs::read_to_string(&input_file)
+        .expect("Could not read file");
+
+    let mut result: Vec<i32> = Vec::new();
+
+    for line in content.lines() {
+        let num: i32 = line.parse()
+            .expect("Could not parse line");
+
+        result.push(num);
+    }
+
+    result
+}
